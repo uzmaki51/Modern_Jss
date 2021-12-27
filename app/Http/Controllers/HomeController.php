46d7544c2f6832c102c45135ed7da811d65371e0
= <=>
2021-12-26 22:23:59 -0500

    ---

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -98,7 +98,7 @@ class HomeController extends Controller {
         $reportList = DecisionReport::where('state','0')->where('ishide',0)->get();
         $noattachments = DecisionReport::where(function($query) {
 			$query->where('attachment',0)->orWhere('attachment',null);
-		})->where('state',0)->where('ishide',0)->get();
+		})->where('ishide',0)->get();
 
 		//return $noattachments;
 
